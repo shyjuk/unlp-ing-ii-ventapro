19e849fa669fa9c5d5150c4cfd0c015b685894a8
damaleos <damaleos@yahoo.com.ar>
2012-05-23 18:13:26 +0000

    Agregado de clases para modulo gestion de productos.

---
+++ src/WEB-INF/src/unlp/info/ingenieriaii/modelo/TipoDeProducto.java
@@ -0,0 +1,22 @@
+package unlp.info.ingenieriaii.modelo;
+
+public class TipoDeProducto extends ObjetoPersistente{
+
+	private String nombre;
+	private String descripcion;
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
