1696229a259ad7fb4153d619faa7f8ada6b19ccb
damaleos <damaleos@yahoo.com.ar>
2012-05-23 18:43:37 +0000

    Agregado de validaciones, codigo objeto persistente

--- src/WEB-INF/src/unlp/info/ingenieriaii/modelo/TipoDeProducto.java
+++ src/WEB-INF/src/unlp/info/ingenieriaii/modelo/TipoDeProducto.java
@@ -1,11 +1,15 @@
 package unlp.info.ingenieriaii.modelo;
 
+import unlp.info.ingenieriaii.web.Validador;
+
 public class TipoDeProducto extends ObjetoPersistente{
 
 	private String nombre;
 	private String descripcion;
 	
-	
+	public boolean esValidoParaCrear()	{
+		return super.esValidoParaCrear() && Validador.esValidoTipoDeProducto(this, this.getErrores());
+	}
 	
 	public String getNombre() {
 		return nombre;
--- src/WEB-INF/src/unlp/info/ingenieriaii/web/Validador.java
+++ src/WEB-INF/src/unlp/info/ingenieriaii/web/Validador.java
@@ -6,11 +6,23 @@ import unlp.info.ingenieriaii.modelo.TipoDeProducto;
 
 public class Validador {
 	
-	public static boolean esValidoTipoDeProducto (TipoDeProducto tipoDeProducto) {
-		HashMap<String, String> errores = new HashMap<String, String>();
-		boolean esValido = (tipoDeProducto != null);
-		
-		return esValido;
+	public static final String ERROR_GENERICO = "GENERICO";
+	
+	public static boolean esValidoTipoDeProducto (TipoDeProducto tipoDeProducto, HashMap<String, String> errores) {
+		errores.clear();
+		if (tipoDeProducto.getNombre() == null || tipoDeProducto.getNombre().length() == 0) {
+			errores.put("nombre", "Complete el campo.");
+			return false;
+		}
+		if (tipoDeProducto.getNombre().length() > 50) {
+			errores.put("nombre", "Ingrese entre 1 y 50 caracteres.");
+			return false;
+		}
+		if (tipoDeProducto.getDescripcion().length() > 100) {
+			errores.put("descripcion", "Ingrese entre 1 y 100 caracteres.");
+			return false;
+		}
+		return true;
 	}
 	
 	
