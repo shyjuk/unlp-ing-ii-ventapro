f67b138bd617a48c222ae9b02a744b737a4f7f26
damaleos <damaleos@yahoo.com.ar>
2012-05-25 21:34:08 +0000

    servlet

---
+++ src/WEB-INF/src/unlp/info/ingenieriaii/web/VentaProServlet.java
@@ -0,0 +1,44 @@
+package unlp.info.ingenieriaii.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VentaProServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -8532993373342222147L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+		String unAttributo = (String) req.getAttribute("nombreAttributo");
+		
+	    try {
+	    	
+	    	// Aca iria la logica de rediccion
+	    	
+	    	if (unAttributo != null && unAttributo.equals("ir")) {
+	    		RequestDispatcher dispatcher =
+	  	    	  req.getRequestDispatcher( "/paginas/buscarTipoProducto.jsp");
+	  	    	  dispatcher.forward(req, resp);
+	    	}
+
+	    } catch (ServletException e) {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+	    } catch (IOException e) {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+	    }
+	  }
+		
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doGet(req, resp);
+	}
+	
+	}
