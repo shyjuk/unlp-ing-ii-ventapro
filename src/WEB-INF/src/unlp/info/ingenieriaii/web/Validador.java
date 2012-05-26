204785d6bf0d5f58ba1ee1c58b6244de12ee678e
anabella234 <anabella234@gmail.com>
2012-05-26 23:25:55 +0000

    Agregue unas validaciones de Producto

--- src/WEB-INF/src/unlp/info/ingenieriaii/web/Validador.java
+++ src/WEB-INF/src/unlp/info/ingenieriaii/web/Validador.java
@@ -3,6 +3,7 @@ package unlp.info.ingenieriaii.web;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import unlp.info.ingenieriaii.modelo.Producto;
 import unlp.info.ingenieriaii.modelo.SucursalUno;
 import unlp.info.ingenieriaii.modelo.TipoDeProducto;
 
@@ -30,6 +31,47 @@ public class Validador {
 		return true;
 	}
 	
+	
+	public static boolean esValidoProducto (Producto producto, HashMap<String, String> errores) {
+		errores.clear();
+		if (producto.getCodigo() == 0) {
+			errores.put("Codigo", "Complete el campo.");
+			return false;
+		}
+		if (producto.getNombre() == null || producto.getNombre().length() == 0) {
+			errores.put("Nombre", "Complete el campo.");
+			return false;
+		}
+		if (producto.getNombre().length() > 50) {
+			errores.put("Nombre", "Ingrese entre 1 y 50 caracteres.");
+			return false;
+		}
+		if (producto.getPrecio() == 0) {
+			errores.put("Precio", "Complete el campo.");
+			return false;
+		}
+		if (producto.getStock() == ' ') {
+			errores.put("Stock", "Complete el campo.");
+			return false;
+		}
+		if (producto.getStockMinimo() == ' ') {
+			errores.put("Stock Minimo", "Complete el campo.");
+			return false;
+		}
+		if (producto.getGarantía() != 'Y' || producto.getGarantía()!=  'N'  ) {
+			errores.put("Garantia", "Complete el campo.");
+			return false;
+		}
+		if (producto.getDescripcion().length() > 100) {
+			errores.put("Descripcion", "Ingrese entre 1 y 100 caracteres.");
+			return false;
+		}
+		if (estaRepetido(producto)) {
+			errores.put("Nombre", "El producto ya existe.");
+		}
+		return true;
+	}
+	
 	private static boolean estaRepetido (TipoDeProducto tipoDeProducto) {
 		ArrayList<TipoDeProducto> todos = SucursalUno.getSingleInstance().getTiposDeProducto();
 		for (TipoDeProducto tipoProducto : todos) {
@@ -41,7 +83,16 @@ public class Validador {
 		return false;
 	}
 	
-	
+	private static boolean estaRepetido (Producto producto) {
+		ArrayList<Producto> todos = SucursalUno.getSingleInstance().getProductos();
+		for (Producto productos : todos) {
+			if (productos.getNombre().equalsIgnoreCase(producto.getNombre()) 
+					&& productos.getId() != producto.getId()) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 
 }
