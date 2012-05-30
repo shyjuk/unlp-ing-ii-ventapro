d929052239e8fc28c7eee49163dd94035dc89740
anabella234 <anabella234@gmail.com>
2012-05-30 16:27:12 +0000

    Agregué la funcion para validar numeros

--- src/paginas/funciones.js
+++ src/paginas/funciones.js
@@ -26,3 +26,11 @@ function changeAllSelection(element, list) {
 		}
 	}
 }
+
+function isNumberKey(evt) {
+         var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+
+          return true;
+       }
