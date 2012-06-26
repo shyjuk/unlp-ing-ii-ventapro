de07b1b0b29ff1f23eb934bf49d1d81ae2b5782c
anabella234 <anabella234@gmail.com>
2012-06-26 15:38:03 +0000

    Modifiqué para que traiga Tipo y Marca

--- src/WEB-INF/sql/productos/productosInactivos.sql
+++ src/WEB-INF/sql/productos/productosInactivos.sql
@@ -2,10 +2,19 @@ delimiter $$
 
 CREATE PROCEDURE `productosInactivos`()
 BEGIN
-    SELECT codigo,nombre,precio,garantia,stock,stockMinimo,
-   	IF(enVenta=0,'No','Si') as enVenta,
- 	descripcion 
-    FROM tbl_productos 
+    SELECT p.codigo,
+    p.nombre,
+    tp.nombre as tipo,
+    m.nombre as marca,
+    p.precio,
+    p.garantia,
+    p.stock,
+    p.stockMinimo,
+   	IF(p.enVenta=1,'Si','No') as enVenta,
+ 	p.descripcion
+    FROM tbl_productos p 
+    INNER JOIN tbl_marcas m ON p.idMarca=m.IdMarca
+    INNER JOIN tbl_tipos_producto tp ON p.idTipoProducto=tp.idTipoProducto 
     WHERE enVenta='No'
     ORDER BY codigo;
 END$$
--- src/WEB-INF/sql/productos/traerProductos.sql
+++ src/WEB-INF/sql/productos/traerProductos.sql
@@ -2,9 +2,18 @@ delimiter $$
 
 CREATE PROCEDURE `traerProductos`()
 BEGIN
-    SELECT codigo,nombre,precio,garantia,stock,stockMinimo,
-   	IF(enVenta=1,'Si','No') as enVenta,
- 	descripcion 
-    FROM tbl_productos 
+	SELECT p.codigo,
+    p.nombre,
+    tp.nombre as tipo,
+    m.nombre as marca,
+    p.precio,
+    p.garantia,
+    p.stock,
+    p.stockMinimo,
+   	IF(p.enVenta=1,'Si','No') as enVenta,
+ 	p.descripcion
+    FROM tbl_productos p 
+    INNER JOIN tbl_marcas m ON p.idMarca=m.IdMarca
+    INNER JOIN tbl_tipos_producto tp ON p.idTipoProducto=tp.idTipoProducto
     ORDER BY codigo, enVenta;
 END$$
